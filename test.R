d1928001f624115e19cba6575c5f31e199993904
mvl22 <martin@lucas-smith.co.uk>
2021-12-31 18:47:40 +0000

    In test.R, avoid warning "In readLines(input, n = 1) : incomplete final line found on 'stdin'"

--- test.R
+++ test.R
@@ -10,7 +10,7 @@ process_results = function(args) {
 # Get data values from STDIN
 # See: https://datafireball.com/2013/10/10/putting-your-r-code-into-pipeline/comment-page-1/
 input = file('stdin', 'r');
-args = readLines(input, n=1)
+args = readLines(input, n=1, warn=FALSE)
 
 # Process the data
 result = process_results(args)
