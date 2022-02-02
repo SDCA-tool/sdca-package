ce927f231812e538cf7189e5e8be1ee7b45de0f4
Malcolm Morgan <mem48@users.noreply.github.com>
2022-02-02 23:10:31 +0000

    Another 3.6 bug?

--- R/measure_infrastucture.R
+++ R/measure_infrastucture.R
@@ -10,14 +10,14 @@
 measure_infrastucture <- function(infra,
                                   path_dem = "D:/GitHub/SDCA-tool/sdca-data-prep/data/UK-dem-50m-4326-Int16.tif"){
   
-  infra <- sf::st_as_sf(infra)
-  sf::st_geometry(infra) <- "geometry"
   
+  # R 3.6 Bug?
+  infra <- as.data.frame(infra)
+  infra <- sf::st_as_sf(infra, crs = 4326)
   
   infra$length <- as.numeric(sf::st_length(infra))
   
   # Get elevations
-  
   elevations <- try(extract_elevations(infra$geometry, path_dem), silent = TRUE)
   if("try-error" %in% class(elevations)){
     elevations <- infra
@@ -30,7 +30,6 @@ measure_infrastucture <- function(infra,
   elevations$width <- 19
   
   elevations <- cut_fill(elevations)
-  
   total_cut <- sum(elevations$volume[elevations$volume > 0])
   total_fill <- sum(elevations$volume[elevations$volume < 0]) * -1
   net_earth_needed <- total_fill - total_cut #-ve materail needs removing +ve material needs to be brought to site
