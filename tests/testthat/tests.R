735c5c580b76687f442d146a244c1327308bd91e
Malcolm Morgan <mem48@users.noreply.github.com>
2022-02-18 12:44:48 +0000

    Additional tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -12,3 +12,17 @@ test_that("Bristol cycling example", {
   res <- process_results(args)
   expect_is(res, "json")
 })
+
+
+# Test via API
+test_that("Cycle across the sea", {
+  dat <- geojson_api(path = "water_and_demand_test.geojson")
+  dat$path_dem <- "dem.tif"
+  dat$path_landcover <- "landcover.tif"
+  dat$path_bedrock <- "bedrock.tif"
+  dat$path_superficial <- "superficial.tif"
+  res <- process_results(dat, local = TRUE)
+  expect_is(res, "json")
+})
+
+
