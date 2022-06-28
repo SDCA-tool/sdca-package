e6d9eb065ff965c9b6ecdbb2fd80c4da487d7a3e
Malcolm Morgan <mem48@users.noreply.github.com>
2022-06-28 12:27:18 +0100

    custom raster locations and check

--- R/send_geojson_to_api.R
+++ R/send_geojson_to_api.R
@@ -2,10 +2,14 @@
 #'
 #' @description Function that take JSON from the API and convert into R format
 #'
-#' @param path path to geojson
+#' @param path path to geojson downloaded from website
 #' @param url API url
 #' @param showinput logical, if TRUE input to R package is returned else resutls
 #'   are returned
+#' @param path_dem path to the terrain raster, defaults to location set by `download_rasters()`
+#' @param path_landcover path to the land cover raster, defaults to location set by `download_rasters()`
+#' @param path_bedrock path to the bedrock raster, defaults to location set by `download_rasters()`
+#' @param path_superficial path to the superficial raster, defaults to location set by `download_rasters()`
 #' @return a named list of data frames and other objects
 #' @examples
 #' \dontrun{
@@ -15,7 +19,11 @@
 
 geojson_api <- function(path, 
                         url = "https://dev.carbon.place/api/v1/locations.json?geojson=",
-                        showinput = TRUE){
+                        showinput = TRUE,
+                        path_dem = paste0(.libPaths()[1],"/sdca/exdata/UKdem.tif"),
+                        path_landcover = paste0(.libPaths()[1],"/sdca/exdata/landcover.tif"),
+                        path_bedrock = paste0(.libPaths()[1],"/sdca/exdata/bedrock.tif"),
+                        path_superficial = paste0(.libPaths()[1],"/sdca/exdata/superficial.tif")){
   # Reag GEOJSON
   #json <- geojsonsf::geojson_sf(path)
   #json <- geojsonsf::sf_geojson(json)
@@ -42,11 +50,27 @@ geojson_api <- function(path,
   dat$desire_lines <- geojsonsf::geojson_sf(dat$desire_lines)
   
   # Change raster paths
-  dat$path_dem <- paste0(.libPaths()[1],"/sdca/exdata/UKdem.tif")
-  dat$path_landcover <- paste0(.libPaths()[1],"/sdca/exdata/landcover.tif")
-  dat$path_bedrock <- paste0(.libPaths()[1],"/sdca/exdata/bedrock.tif")
-  dat$path_superficial <- paste0(.libPaths()[1],"/sdca/exdata/superficial.tif")
-  
+  if(file.exists(path_dem)){
+    dat$path_dem <- path_dem
+  } else{
+    warning("could not find terrain raster")
+  }
+  if(file.exists(path_landcover)){
+    dat$path_landcover <- path_landcover
+  } else{
+    warning("could not find land cover raster")
+  }
+  if(file.exists(path_bedrock)){
+    dat$path_bedrock <- path_bedrock
+  } else{
+    warning("could not find bedrock raster")
+  }
+  if(file.exists(path_superficial)){
+    dat$path_superficial <- path_superficial
+  } else{
+    warning("could not find superficial raster")
+  }
+
   return(dat)
   
 }
